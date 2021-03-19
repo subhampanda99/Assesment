8659b3beeaaeb9e4a7e211e51ea916b55c47886f
Subham Panda <subhampanda2899@gmail.com>
2021-03-19 18:45:17 +0530

    Errors solved

--- pdf.js
+++ pdf.js
@@ -1,16 +1,17 @@
 window.onload = function () {
     document.getElementById("download")
         .addEventListener("click", () => {
-            const invoice = this.document.getElementById("invoice");
-            console.log(invoice);
+            const invoice = this.document.getElementById("tabs");
+            console.log(tabs);
             console.log(window);
             var opt = {
                 margin: 1,
                 filename: 'myfile.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
-                html2canvas: { scale: 2 },
+                html2canvas: { scale: 3 },
                 jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
             };
             html2pdf().from(invoice).set(opt).save();
         })
 }
+
